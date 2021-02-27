37a59b05f4e853d5cdc6955416f59c96a307d282
Behnam Margan <behnammargan@gmail.com>
2021-02-27 23:43:49 +0330

    minor

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || '8000'
  * @apiSuccess (200) {String} message the hello {name} message
  */
 app.get('/hello/:name', (req, res) => {
-  console.log('Log request: ', req.params.name)
+  console.log('Log requests: ', req.params.name)
   return res.send({
     message: `Hello ${req.params.name}`
   })
