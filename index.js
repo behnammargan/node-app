56fae06d93ccd4c9ef83d12256910407356c81ea
Behnam <behnammargan@gmail.com>
2021-03-13 22:08:14 +0330

    port problem

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || '8000'
+const PORT = process.env.PORT || '8001'
 
 /**
  * @api {get} /hello/{name} Prints "Hello {name}"
